27a371ef554b5cd0c6b7e4493678c579bf5579c9
kayyamervee <kayaamervee01@gmail.com>
2024-10-27 04:50:44 +0300

    configuration properties has been updated.

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -1,11 +1,48 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.GWD;
 
-public class Login extends ParentPage{
-
-    public Login() {
-        PageFactory.initElements(GWD.getDriver(),this);
+public class Login extends ParentPage {
+        public Login() {
+        PageFactory.initElements(GWD.getDriver(), this);
     }
+
+    @FindBy(linkText = "Sign In")
+    public WebElement signInLink;
+
+    @FindBy(xpath = "//span[text()='Customer Login']")
+    public WebElement customerLogin;
+
+    @FindBy(name = "login[username]")
+    public WebElement emailBox;
+
+    @FindBy(name = "login[password]")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "(//div[@class='primary']/button)[1]")
+    public WebElement signInButton;
+
+    @FindBy(xpath = "//h1[@class='page-title']/span")
+    public WebElement myAccountText;
+
+    @FindBy(xpath = "//div[@class='box-content']/p")
+    public WebElement accountDetails;
+
+    @FindBy(xpath = "//a[@class='logo']")
+    public WebElement logoClick;
+
+    @FindBy(xpath = "(//span[contains(text(),'Welcome')])[1]")
+    public WebElement welcomeText;
+
+    @FindBy(xpath = "//a[@class='action remind']/span")
+    public WebElement forgotPasswordButton;
+
+    @FindBy(xpath = "//h1[@class='page-title']/span")
+    public WebElement forgetPasswordText;
+
+    @FindBy(xpath = "//div[contains(text(),'Please enter your email address')]")
+    public WebElement forgotPasswordConfirmText;
 }
