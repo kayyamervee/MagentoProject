7bd9a0d4700c1fc041cef156bba9ae1c6eae993b
SefaKahramannn <sefakahraman0001@gmail.com>
2024-10-29 13:57:42 +0300

    WishList changed

---
+++ src/test/java/pages/WishListPOM.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.GWD;
+
+import java.util.List;
+
+public class WishListPOM extends ParentPage {
+
+    public WishListPOM() {
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//ul[@class='header links']//button)[1]")
+    public WebElement dropDownMenuBtn;
+
+    @FindBy(xpath = "(//ul[@class='header links']//a)[1]")
+    public WebElement myAccountBtn;
+
+    @FindBy(css = "[class='product-item-name']")
+    public List<WebElement> products;
+
+    @FindBy(css = "[class='product-item-link']")
+    public WebElement wishListProduct;
+
+    @FindBy(xpath = "//a[@class='action towishlist']")
+    public List<WebElement> wishListBtn;
+
+    @FindBy(linkText = "My Wish List")
+    public WebElement myAccountWishListBtn;
+
+    @FindBy(css = "[data-bind='html: $parent.prepareMessageForHtml(message.text)']")
+    public WebElement wishListConfirmMessage;
+
+    @FindBy(css = "a[title='Remove Item']")
+    public WebElement deleteWishListBtn;
+
+    public WebElement getWebElement(String strWebElement) {
+        switch (strWebElement) {
+            case "myAccountButton":
+                return this.myAccountBtn;
+            case "wishListButton":
+                return this.myAccountWishListBtn;
+            case "dropDownMenu":
+                return this.dropDownMenuBtn;
+            case "removeFavoritesButton":
+                return this.deleteWishListBtn;
+        }
+        return null;
+    }
+
+}
